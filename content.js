31572e3cf1a7eac99d7cf09d0294ccdad03bd6c5
EdSchernau <43827598+EdSchernau@users.noreply.github.com>
2018-10-03 15:44:18 -0400

    Initial commit
    Support for vBulletin.   Removes "Tapatalk" signatures and posts from ignored users.

---
+++ content.js
@@ -0,0 +1,39 @@
+if ( location.href.match(/showthread/g) ) {
+	// because the extension applies to all URLs, we have to filter
+	// showthread.php is a vBulletin thing, so we're almost there
+
+	console.log("Forum Fixer: showthread URL match, checking for vBulletin.");
+
+	var x = document.body.innerHTML;
+	
+	//simple test for vBulletin anywhere in the page, then strip sections
+	if ( x.match(/vBulletin/g) ) {
+		console.log("vBulletin found, stripping ignored user posts.");
+
+	//strip "Sent from my $foo using Tapatalk" spam
+	document.body.innerHTML = x.replace(/Sent from my .* using Tapatalk/i,"");
+
+
+
+
+	//strip ignored users' posts
+	var ignored = document.getElementsByClassName("postbitignored");
+	var i;
+	var comments = ignored.length + " comment";
+	if ( ignored.length>1 || ignored.length==0) { 
+		comments = comments + "s";
+		// eyecandy
+	}
+
+	console.log("Forum Fixer: removing " + comments + ".");
+
+	for (i = ignored.length-1; i >=0; i--) {
+		ignored[i].parentNode.removeChild(ignored[i]);
+	}
+
+	
+	}	
+}
+else {
+	console.log("Forum Fixer: no applicable URLs detected.");
+}
